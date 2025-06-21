1c56c229c6a65aeab5d349272c8d8fa154d3d604
gonzalo <you@example.com>
2025-06-20 20:42:42 -0500

    correccion redirectview

--- businessapp/urls.py
+++ businessapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/login/', permanent=False), name='root'),
