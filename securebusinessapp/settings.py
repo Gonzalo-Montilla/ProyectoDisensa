8285f62030f705144e42e76855eb30a0719adb30
gonzalo <you@example.com>
2025-06-25 20:50:51 -0500

    correcion de static files

--- securebusinessapp/settings.py
+++ securebusinessapp/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'businessapp/static']
 
 # Media files (si los usas)
